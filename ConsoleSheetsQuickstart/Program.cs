562033bf805546acb7f0ef5253a85badec5c37cf
Ricardo P Avila <rpaa01@gmail.com>
2019-01-04 23:03:03 -0600

    removed unnecessary type

--- ConsoleSheetsQuickstart/Program.cs
+++ ConsoleSheetsQuickstart/Program.cs
@@ -41,38 +41,32 @@ namespace ConsoleSheetsQuickstart
             var methodOpenSession = googleSheetsType.GetMethod("OpenSession");
             var methodReadCellValues = googleSheetsType.GetMethod("ReadCellValues");
             var methodWriteCellValues = googleSheetsType.GetMethod("WriteCellValues");
-
-            var parametersArray = new object[] { spreadsheetId };
-            var session = methodOpenSession.Invoke(gsInstance, parametersArray);
+            
+            var session = methodOpenSession.Invoke(gsInstance, new object[] { spreadsheetId });
 
             //  Creating a CellAddress instance
-            Type cellAddressType = testAssembly.GetType("GSpreadSheet.CellAddress", true);
-            Type cellAddressValueType = testAssembly.GetType("GSpreadSheet.CellAddressWithValue", true);
-            Type executionResultWitDataType = testAssembly.GetType("GSpreadSheet.ExecutionResultWithData`1", true);
+            var cellAddressType = testAssembly.GetType("GSpreadSheet.CellAddress", true);
+            var cellAddressValueType = testAssembly.GetType("GSpreadSheet.CellAddressWithValue", true);
 
-            Type listType = typeof(List<>);
+            var listType = typeof(List<>);
             Type[] typeArgs = { cellAddressType };
-            Type listGenericType = listType.MakeGenericType(typeArgs);
-            object listGenericInstance = Activator.CreateInstance(listGenericType);
-            object instanceCellAddress = Activator.CreateInstance(cellAddressType, "", "A8");
-            MethodInfo methodAdd = listGenericType.GetMethod("Add");
+            var listGenericType = listType.MakeGenericType(typeArgs);
+            var listGenericInstance = Activator.CreateInstance(listGenericType);
+            var instanceCellAddress = Activator.CreateInstance(cellAddressType, "", "A8");
+            var methodAdd = listGenericType.GetMethod("Add");
             methodAdd.Invoke(listGenericInstance, new object[] { instanceCellAddress });
 
             //  Calling a ReadCellValues function
-            //MethodInfo methodReadCellValues = googleSheetsType.GetMethod("ReadCellValues");
-            //executionResult = methodReadCellValues.Invoke(gsInstance, new object[] { session, listGenericInstance });
-            //bool hasErrors = DisplayErrors(executionResult);
-            //if (!hasErrors)
-            //{
-            //    int i = 1;
-            //    foreach (dynamic row in executionResult.Data)
-            //    {
-            //        var address = row.Address;
-            //        var sheetName = row.SheetName;
-            //        var val = row.Value;
-            //        Console.WriteLine("{0}) - {1}, {2}, {3}", i++, sheetName, address, val);
-            //    }
-            //}
+            dynamic executionResult = methodReadCellValues.Invoke(gsInstance, new object[] { session, listGenericInstance });
+            bool hasErrors = DisplayErrors(executionResult);
+            if (!hasErrors)
+            {
+                int i = 1;
+                foreach (dynamic row in executionResult.Data)
+                {
+                    Console.WriteLine("{0}) - {1}, {2}, {3}", i++, row.SheetName, row.Address, row.Value);
+                }
+            }
 
             //  Creating a CellAddressWithValue instance
             typeArgs[0] = cellAddressValueType;
@@ -87,7 +81,6 @@ namespace ConsoleSheetsQuickstart
             methodAdd.Invoke(listGenericInstance, new object[] { instanceCellAddressValue });
 
             //  Calling a WriteCellValues function
-            //MethodInfo methodWriteCellValues = googleSheetsType.GetMethod("WriteCellValues");
             //executionResult = methodWriteCellValues.Invoke(gsInstance, new object[] { session, listGenericInstance });
             //DisplayErrors(executionResult);
 
